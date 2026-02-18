f6b77ef58961863d3a29ab85578bb854e49b0472
Sharath s <144437869+Sharathsree0@users.noreply.github.com>
2026-02-18 17:07:43 +0530

    Delete server directory

--- server/src/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { rephraseText, summarizesText,  } from "../controller/aiController.js";
-
-const aiRouter= express.Router();
-
-aiRouter.post("/rephrase",rephraseText);
-aiRouter.post("/summarizes",summarizesText)
-
-export default aiRouter;
